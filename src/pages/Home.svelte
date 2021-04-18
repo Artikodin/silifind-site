106e7c181bf85f244f47351992de6a5952715569
Artium Wasse <artium.wasse@vimcar.com>
2021-04-18 16:54:22 +0200

    feat: add pages

---
+++ src/pages/Home.svelte
@@ -0,0 +1,10 @@
+<script>
+  import PageLayout from "../components/PageLayout.svelte";
+  import ProductList from "../components/ProductList.svelte";
+  import CaptationLayout from "../components/CaptationLayout.svelte";
+</script>
+
+<PageLayout>
+  <CaptationLayout />
+  <ProductList />
+</PageLayout>
---
+++ src/pages/Product.svelte
@@ -0,0 +1,8 @@
+<script>
+  import PageLayout from "../components/PageLayout.svelte";
+  import ProductDetail from "../components/ProductDetail.svelte";
+</script>
+
+<PageLayout>
+  <ProductDetail />
+</PageLayout>
